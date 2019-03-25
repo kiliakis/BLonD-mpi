895660ae9700e540186a7bb13b6f2893003819f7
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2019-03-25 14:40:18 +0100

    minor changes

--- scripts/scan/scan_mpi_sps.py
+++ scripts/scan/scan_mpi_sps.py
@@ -19,26 +19,26 @@ job_name_form = '_p{}_b{}_s{}_t{}_w{}_o{}_N{}_r{}_m{}_seed{}_approx{}_mpi{}'
 
 configs = {
 
-    'SPS-rand-72B-4MPPB-approx2': {
-        'exe': cycle([yc['exe_home'] + 'SPS_main_random.py']),
-        'p': cycle([4000000]),
-        'b': cycle([48]),  # 72
-        's': cycle([1408]),
-        't': cycle([43349]),  # 4000
-        'm': cycle([50]),
-        'reduce': cycle([1]),
-        'load': cycle([0.0]),
-        'mtw': cycle([0]),
-        'approx': cycle([2]),
-        'timing': cycle(['']),  # otherwise pass -time
-        'seed': [0] * 4 + [1] * 4 + [2] * 4 + [3] * 4 + [4] * 4 + [5] * 4,
-        'w': []
-        + [2, 4, 8, 16] * 6,
-        'o': cycle([10]),
-        'time': cycle([1000]),
-        'mpi': cycle(['mpich3']),
-        'partition': cycle(['be-short'])
-    },
+    # 'SPS-rand-72B-4MPPB-approx2': {
+    #     'exe': cycle([yc['exe_home'] + 'SPS_main_random.py']),
+    #     'p': cycle([4000000]),
+    #     'b': cycle([48]),  # 72
+    #     's': cycle([1408]),
+    #     't': cycle([43349]),  # 4000
+    #     'm': cycle([50]),
+    #     'reduce': cycle([1]),
+    #     'load': cycle([0.0]),
+    #     'mtw': cycle([0]),
+    #     'approx': cycle([2]),
+    #     'timing': cycle(['']),  # otherwise pass -time
+    #     'seed': [0] * 4 + [1] * 4 + [2] * 4 + [3] * 4 + [4] * 4 + [5] * 4,
+    #     'w': []
+    #     + [2, 4, 8, 16] * 6,
+    #     'o': cycle([10]),
+    #     'time': cycle([1000]),
+    #     'mpi': cycle(['mpich3']),
+    #     'partition': cycle(['be-short'])
+    # },
 
     # 'SPS-rand-72B-4MPPB-approx1': {
     #     'exe': cycle([yc['exe_home'] + 'SPS_main_random.py']),
@@ -62,109 +62,6 @@ configs = {
     #     'mpi': cycle(['mpich3']),
     #     'partition': cycle(['be-long'])
     # },
-
-
-    # 'SPS-b72-4MPPB-t10k-approx-time-3': {
-    #     'exe': cycle([home + '/__EXAMPLES/main_files/SPS_main_random.py']),
-    #     'p': cycle([4000000]),
-    #     'b': cycle([72]), # 72
-    #     's': cycle([1408]),
-    #     't': cycle([10000]), # 4000
-    #     'm': cycle([0]),
-    #     'reduce': cycle([1]),
-    #     'load': cycle([0.0]),
-    #     'mtw': cycle([0]),
-    #     'approx': cycle([2]),
-    #     'timing': cycle(['-time']), # otherwise pass -time
-    #     'seed': cycle([0]),
-    #     'w': []
-    #     + [16],
-    #     # + [1, 2, 4, 8, 16],
-    #     # + list(np.arange(2, 17, 2)),
-    #     # list(np.arange(2, 9, 1)),
-    #     'o': cycle([10]),
-    #     # + [10]*8,
-    #     'time': cycle([180]),
-    #     'partition': cycle(['be-short']),
-    #     # 'repeats': cycle([5])
-    # },
-
-    # 'SPS-b72-4MPPB-t10k-red2-time-2': {
-    #     'exe': cycle([home + '/__EXAMPLES/main_files/SPS_main_random.py']),
-    #     'p': cycle([4000000]),
-    #     'b': cycle([72]), # 72
-    #     's': cycle([1408]),
-    #     't': cycle([5000]), # 4000
-    #     'm': cycle([0]),
-    #     'reduce': cycle([2]),
-    #     'load': cycle([0.0]),
-    #     'mtw': cycle([0]),
-    #     'approx': cycle([1]),
-    #     'timing': cycle(['-time']), # otherwise pass -time
-    #     'seed': cycle([0]),
-    #     'w': []
-    #     # + [1, 2, 4, 8, 16],
-    #     + list(np.arange(2, 17, 2)),
-    #     # list(np.arange(2, 9, 1)),
-    #     'o': cycle([10]),
-    #     # + [10]*8,
-    #     'time': cycle([180]),
-    #     'partition': cycle(['be-short']),
-    #     # 'repeats': cycle([5])
-    # },
-
-
-    # 'SPS-b72-4MPPB-t10k-red3-time-3': {
-    #     'exe': cycle([home + '/__EXAMPLES/main_files/SPS_main_random.py']),
-    #     'p': cycle([4000000]),
-    #     'b': cycle([72]), # 72
-    #     's': cycle([1408]),
-    #     't': cycle([5000]), # 4000
-    #     'm': cycle([0]),
-    #     'reduce': cycle([3]),
-    #     'load': cycle([0.0]),
-    #     'mtw': cycle([0]),
-    #     'approx': cycle([1]),
-    #     'timing': cycle(['-time']), # otherwise pass -time
-    #     'seed': cycle([0]),
-    #     'w': []
-    #     # + [1, 2, 4, 8, 16],
-    #     + list(np.arange(2, 17, 2)),
-    #     # list(np.arange(2, 9, 1)),
-    #     'o': cycle([10]),
-    #     # + [10]*8,
-    #     'time': cycle([180]),
-    #     'partition': cycle(['be-short']),
-    #     # 'repeats': cycle([5])
-    # },
-
-
-    # 'SPS-b72-4MPPB-t10k-red4-time-3': {
-    #     'exe': cycle([home + '/__EXAMPLES/main_files/SPS_main_random.py']),
-    #     'p': cycle([4000000]),
-    #     'b': cycle([72]), # 72
-    #     's': cycle([1408]),
-    #     't': cycle([5000]), # 4000
-    #     'm': cycle([0]),
-    #     'reduce': cycle([4]),
-    #     'load': cycle([0.0]),
-    #     'mtw': cycle([0]),
-    #     'approx': cycle([1]),
-    #     'timing': cycle(['-time']), # otherwise pass -time
-    #     'seed': cycle([0]),
-    #     'w': []
-    #     # + [1, 2, 4, 8, 16],
-    #     + list(np.arange(2, 17, 2)),
-    #     # list(np.arange(2, 9, 1)),
-    #     'o': cycle([10]),
-    #     # + [10]*8,
-    #     'time': cycle([180]),
-    #     'partition': cycle(['be-short']),
-    #     # 'repeats': cycle([5])
-    # },
-
-
-
 
     # 'SPS-b72-4MPPB-t10k-mpich3': {
     #     'exe': cycle([yc['exe_home'] + 'SPS_main_random.py']),
@@ -233,10 +130,77 @@ configs = {
     #     # 'repeats': cycle([5])
     # },
 
+    'SPS-weak-scale-mpich3': {
+        'exe': cycle([yc['exe_home'] + 'SPS_main_random.py']),
+        # 'p': cycle([4000000]),
+        'p': [1e6, 2e6, 4e6, 3e6, 4e6, 2.5e6, 3e6, 3.5e6, 4e6],
+        # 'b': cycle([72]), # 72
+        'b': [18, 18, 18, 36, 36, 72, 72, 72, 72], # 72
+        's': cycle([1408]),
+        't': cycle([10000]), # 4000
+        'm': cycle([0]),
+        'reduce': cycle([1]),
+        'load': cycle([0.0]),
+        'mtw': cycle([0]),
+        'approx': cycle([0]),
+        'timing': cycle(['-time']), # otherwise pass -time
+        'seed': cycle([0]),
+        'w': [1, 2, 4, 6, 8, 10, 12, 14, 16],
+        # + list(np.arange(2, 17, 2)),
+        'o': cycle([10]),
+        'mpi': cycle(['mpich3']),
+        'time': cycle([90]),
+        'partition': cycle(['be-short']),
+    },
+
+    'SPS-weak-scale-openmpi3': {
+        # 'p': cycle([4000000]),
+        'p': [1e6, 2e6, 4e6, 3e6, 4e6, 2.5e6, 3e6, 3.5e6, 4e6],
+        # 'b': cycle([72]), # 72
+        'b': [18, 18, 18, 36, 36, 72, 72, 72, 72], # 72
+        's': cycle([1408]),
+        't': cycle([10000]), # 4000
+        'm': cycle([0]),
+        'reduce': cycle([1]),
+        'load': cycle([0.0]),
+        'mtw': cycle([0]),
+        'approx': cycle([0]),
+        'timing': cycle(['-time']), # otherwise pass -time
+        'seed': cycle([0]),
+        'w': [1, 2, 4, 6, 8, 10, 12, 14, 16],
+        # + list(np.arange(2, 17, 2)),
+        'o': cycle([10]),
+        'mpi': cycle(['openmpi3']),
+        'time': cycle([90]),
+        'partition': cycle(['be-short']),
+    },
+
+    'SPS-weak-scale-mvapich2': {
+        # 'p': cycle([4000000]),
+        'p': [1e6, 2e6, 4e6, 3e6, 4e6, 2.5e6, 3e6, 3.5e6, 4e6],
+        # 'b': cycle([72]), # 72
+        'b': [18, 18, 18, 36, 36, 72, 72, 72, 72], # 72
+        's': cycle([1408]),
+        't': cycle([10000]), # 4000
+        'm': cycle([0]),
+        'reduce': cycle([1]),
+        'load': cycle([0.0]),
+        'mtw': cycle([0]),
+        'approx': cycle([0]),
+        'timing': cycle(['-time']), # otherwise pass -time
+        'seed': cycle([0]),
+        'w': [1, 2, 4, 6, 8, 10, 12, 14, 16],
+        # + list(np.arange(2, 17, 2)),
+        'o': cycle([10]),
+        'mpi': cycle(['mvapich2']),
+        'time': cycle([90]),
+        'partition': cycle(['be-short']),
+    },
+
 }
 
 
-repeats = 1
+repeats = 10
 
 
 total_sims = repeats * \
