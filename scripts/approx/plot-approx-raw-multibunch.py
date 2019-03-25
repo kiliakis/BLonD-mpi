2caba2a1a4d179e880621b0655568f4782945358
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2019-03-25 11:03:13 +0100

    improved bunch detection method

--- scripts/approx/plot-approx-raw-multibunch.py
+++ scripts/approx/plot-approx-raw-multibunch.py
@@ -23,14 +23,14 @@ parser.add_argument('-i', '--indir', type=str, default=None,
 parser.add_argument('-o', '--outdir', type=str, default=None,
                     help='Directory to store the results.')
 
-# parser.add_argument('-a', '--approx', type=int, default=1, choices=[1,2],
-#                     help='Approximation method: 1 --> Global reduce, 2 --> Scale histo')
-
 parser.add_argument('-turns', '--turns', type=int, default=None,
                     help='Last turn to plot (default: plot all the turns).')
 
 parser.add_argument('-p', '--points', type=int, default=100,
                     help='Num of points in the plot. Default: 100')
+
+parser.add_argument('-m', '--minparts', type=float, default=0.01,
+                    help='Percent of particles in a bin to be considered a new bunch. Default: 0.01')
 
 parser.add_argument('-t', '--ts', type=str, default=['1'], nargs='+',
                     help='Running mean window. Default: [1]')
@@ -70,25 +70,42 @@ def remove_nan(x):
         i += 1
 
 
+def detect_bunches(y, min_parts):
+    bunches = []
+    phase = 'start'
+    i = 0
+    while i < len(y):
+        # First we look for a slice with more that 1% parts for 10
+        # slices in a row
+        if phase == 'start':
+            if (y[i] >= min_parts) and (np.min(y[i:i+10]) > min_parts):
+                bunches.append([i, i])
+                phase = 'end'
+        # Finally we look for the end of the bunches, a region
+        # with very few particles per slice for some slices in a row
+        elif phase == 'end':
+            if (y[i] < min_parts) and (np.max(y[i:i+10]) < min_parts):
+                bunches[-1][1] = i
+                phase = 'start'
+        i += 1
+    return bunches
+
+
 if __name__ == '__main__':
     args = parser.parse_args()
-    args = vars(args)
+    # args = vars(args)
 
-    last_t = args['turns']
-    indir = args['indir']
-    outdir = args['outdir']
-    points = args['points']
-    tss = args['ts']
+    outdir = args.outdir
 
     if not os.path.exists(outdir):
         os.makedirs(outdir)
     plot_dir = {}
-    for dirpath, dirnames, filenames in os.walk(indir):
+    for dirpath, dirnames, filenames in os.walk(args.indir):
         if 'monitor.h5' not in filenames:
             continue
 
         particles = dirpath.split('_p')[1].split('_')[0]
-        bunches = dirpath.split('_b')[1].split('_')[0]
+        num_bunches = dirpath.split('_b')[1].split('_')[0]
         seed = dirpath.split('_seed')[1].split('_')[0]
         monitor_intv = dirpath.split('_m')[1].split('_')[0]
         red = dirpath.split('_r')[1].split('_')[0]
@@ -111,7 +128,6 @@ if __name__ == '__main__':
             continue
 
         x = inh5file['default']['turns'].value
-        # intv = int(np.ceil(len(x)/points))
         for key in inh5file['default'].keys():
             if (key in not_plot):
                 continue
@@ -152,7 +168,7 @@ if __name__ == '__main__':
     # continue here, I need to iterate over the errors, create a figure for each
     # iterate over the reduce values, add an error plot line for each acording to the intv etc
 
-    for ts in tss:
+    for ts in args.ts:
         # filename = outfile + '/ts' + str(ts) + '.h5'
         if not os.path.exists(os.path.dirname(outdir + '/ts' + ts+'/')):
             os.makedirs(os.path.dirname(outdir + '/ts' + ts+'/'))
@@ -163,49 +179,54 @@ if __name__ == '__main__':
             if error == 'profile':
                 fig, ax_arr = plt.subplots(
                     nrows=1, ncols=2, figsize=(4, 4), sharey=True)
-                fig.suptitle('Ts: {}, Variable: {}'.format(ts, error), fontsize=8)
+                fig.suptitle('Ts: {}, Variable: {}'.format(
+                    ts, error), fontsize=8)
 
                 for approx_knob, data in plot_dir[error].items():
                     x = data['turns']
                     y = data['sum'] / data['num']
                     ymin = data['min']
                     ymax = data['max']
-                    intv = int(np.ceil(len(x)/points))
+                    intv = int(np.ceil(len(x)/args.points))
                     label = 'apprx{}_{}'.format(approx, approx_knob)
                     marker = markers.get(
                         'apprx{}_{}'.format(approx, approx_knob), None)
                     # Here I need a way to isolate the first and the last bunch only
                     # A tuple of three indices, start, max, end
-                    bunch = []
-                    min_parts = np.max(y)/100.
-                    i = 0
-                    phase = 'start'
-                    while i < len(y):
-                        # First we look for a slice with more that 1% parts for 10
-                        # slices in a row
-                        if phase == 'start':
-                            if (y[i] >= min_parts) and (np.min(y[i:i+10]) > min_parts):
-                                bunch.append([i, i])
-                                phase = 'end'
-                        # Finally we look for the end of the bunch, a region
-                        # with very few particles per slice for some slices in a row
-                        elif phase == 'end':
-                            if (y[i] < min_parts) and (np.max(y[i:i+10]) < min_parts):
-                                bunch[-1][1] = i
-                                phase = 'start'
-                        i += 1
-                    if (len(bunch) != int(bunches)):
-                        print('WARNING: Knob {}, detected {} instead of {} bunches.'.format(
-                            approx_knob, len(bunch), bunches))
 
-                    bunch_idx = [0, -1]
+                    left = 0
+                    right = 100
+                    max_parts = np.max(y)
+                    bunches = []
+                    percent = args.minparts
+                    while left < right:
+                        mid = int((left+right)//2)
+                        min_parts = mid/1000. * max_parts
+                        temp = detect_bunches(y, min_parts)
+                        if (len(temp) < int(num_bunches)):
+                            right = mid - 1
+                        elif (len(temp) > int(num_bunches)):
+                            left = mid + 1
+                        else:
+                            bunches = temp
+                            right = mid - 1
+                            percent = mid / 10.
+
+                    if len(bunches) == 0:
+                        bunches = detect_bunches(y, max_parts * args.minparts)
+                        print('WARNING: Knob {}, could not detect {} bunches. Using {}% {} bunches' +
+                              ' were detected.'.format(approx_knob, num_bunches, args.minparts, len(bunches)))
+                    else:
+                        print('Knob {}, detected {} bunches with {}%'.format(approx_knob, len(bunches), percent))
+
+                    bunch_idx=[0, -1]
                     for idx in bunch_idx:
                         plt.sca(ax_arr[idx])
-                        b = bunch[idx]
-                        x1 = np.arange(b[0], b[1])
-                        y1 = y[x1]
-                        ymin1 = ymin[x1]
-                        ymax1 = ymax[x1]
+                        b=bunches[idx]
+                        x1=np.arange(b[0], b[1])
+                        y1=y[x1]
+                        ymin1=ymin[x1]
+                        ymax1=ymax[x1]
                         # x1 = np.arange(len(x1))
                         if (approx_knob == '1'):
                             plt.fill_between(
@@ -244,8 +265,8 @@ if __name__ == '__main__':
 
             else:
 
-                lines = 0
-                fig = plt.figure(figsize=(4, 4))
+                lines=0
+                fig=plt.figure(figsize=(4, 4))
 
                 plt.grid()
                 plt.title('Ts: {}, Variable: {}'.format(ts, error), fontsize=8)
@@ -253,21 +274,21 @@ if __name__ == '__main__':
                 plt.ylabel('Raw value', fontsize=8)
 
                 for approx_knob, data in plot_dir[error].items():
-                    x = data['turns']
-                    y = data['sum'] / data['num']
-                    ymin = data['min']
-                    ymax = data['max']
-                    intv = int(np.ceil(len(x)/points))
-                    label = 'apprx{}_{}'.format(approx, approx_knob)
-                    marker = markers.get(
+                    x=data['turns']
+                    y=data['sum'] / data['num']
+                    ymin=data['min']
+                    ymax=data['max']
+                    intv=int(np.ceil(len(x)/args.points))
+                    label='apprx{}_{}'.format(approx, approx_knob)
+                    marker=markers.get(
                         'apprx{}_{}'.format(approx, approx_knob), None)
-                    y = running_mean(y, int(ts))
-                    ymin = running_mean(ymin, int(ts))
-                    ymax = running_mean(ymax, int(ts))
-                    x = x[:len(y):intv]
-                    y = y[::intv]
-                    ymin = ymin[::intv]
-                    ymax = ymax[::intv]
+                    y=running_mean(y, int(ts))
+                    ymin=running_mean(ymin, int(ts))
+                    ymax=running_mean(ymax, int(ts))
+                    x=x[:len(y):intv]
+                    y=y[::intv]
+                    ymin=ymin[::intv]
+                    ymax=ymax[::intv]
                     if (approx_knob == '1'):
                         plt.fill_between(
                             x, ymin, ymax, facecolor='0.6', interpolate=True)
@@ -289,7 +310,7 @@ if __name__ == '__main__':
                 plt.yticks(fontsize=8)
                 plt.ticklabel_format(axis='y', style='sci', scilimits=(0, 0))
                 plt.gca().tick_params(pad=1, top=1, bottom=1, left=1,
-                      direction='inout', length=3, width=0.5)
+                                      direction='inout', length=3, width=0.5)
                 plt.legend(loc='upper left', fancybox=True, fontsize=8,
                                ncol=(lines+2)//3, columnspacing=1,
                                labelspacing=0.1, borderpad=0.2, framealpha=0.5,
@@ -297,10 +318,10 @@ if __name__ == '__main__':
 
             plt.tight_layout()
             plt.subplots_adjust(wspace=0.0)
-            
+
             for outfile in outfiles:
                 save_and_crop(fig, outfile, dpi=900, bbox_inches='tight')
                 # fig.savefig(outfile, dpi=900, bbox_inches='tight')
-            if args['show'] is True:
+            if args.show is True:
                 plt.show()
             plt.close()
