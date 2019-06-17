d6b051e5d441394f56f65d12118e6687c9e5680a
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2019-06-17 14:44:33 +0200

    fixed bug in extract

--- scripts/plot/multiple_files_speedup_lb.py
+++ scripts/plot/multiple_files_speedup_lb.py
@@ -14,181 +14,96 @@ this_filename = sys.argv[0].split('/')[-1]
 project_dir = this_directory + '../../'
 res_dir = project_dir + 'results/'
 images_dir = res_dir + 'plots/redistribute/'
-# images_dir = res_dir + '/blond-meeting/'
 
 if not os.path.exists(images_dir):
     os.makedirs(images_dir)
 
-case = 'ps'
+case = 'lhc'
 
 config = {
-
     'files': {
-        # res_dir+'raw/SPS-b72-4MPPB-t10k-mpich3/comm-comp-report.csv': {
-        #     'key': 'sps-mpich3',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
+    #     '{}/raw/{}/mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
+    #         'key': '{}'.format(case),
+    #         'lines': {
+    #             'mpi': ['mpich3', 'mvapich2'],
+    #             'lb': ['interval', 'reportonly'],
+    #             'approx': ['0', '2'],
+    #             'lba': ['100', '500'],
+    #             'type': ['total'],
+    #         }
+    #     },
+    #     '{}/raw/{}/mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
+    #         'key': '{}'.format(case),
+    #         'lines': {
+    #             'mpi': ['mpich3', 'mvapich2'],
+    #             'lb': ['interval', 'reportonly'],
+    #             'approx': ['0', '2'],
+    #             'lba': ['100', '500'],
+    #             'type': ['total'],
+    #         }
+    #     },
+    #     '{}/raw/{}/lb-mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
+    #         'key': '{}'.format(case),
+    #         'lines': {
+    #             'mpi': ['mpich3', 'mvapich2'],
+    #             'lb': ['interval', 'reportonly'],
+    #             'approx': ['0', '2'],
+    #             'lba': ['100', '500'],
+    #             'type': ['total'],
+    #         }
+    #     },
+    #     '{}/raw/{}/lb-mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
+    #         'key': '{}'.format(case),
+    #         'lines': {
+    #             'mpi': ['mpich3', 'mvapich2'],
+    #             'lb': ['interval', 'reportonly'],
+    #             'approx': ['0', '2'],
+    #             'lba': ['100', '500'],
+    #             'type': ['total'],
+    #         }
+    #     },
 
-        # res_dir+'raw/SPS-b72-4MPPB-t10k-mvapich2/comm-comp-report.csv': {
-        #     'key': 'sps-mvapich2',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-        # res_dir+'raw/SPS-b72-4MPPB-t10k-openmpi3/comm-comp-report.csv': {
-        #     'key': 'sps-openmpi3',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-        # res_dir+'raw/EX01-mpich3-approx2/comm-comp-report.csv': {
-        #     'key': 'ex01-mpich3apprx',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
 
-        # res_dir+'raw/EX01-lb-mpich3-approx2/comm-comp-report.csv': {
-        #     'key': 'ex01-lbmpich3apprx',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # res_dir+'raw/EX01-mpich3/comm-comp-report.csv': {
-        #     'key': 'ex01-mpich3',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-        # res_dir+'raw/EX01-lb-mpich3/comm-comp-report.csv': {
-        #     'key': 'ex01-lbmpich3',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # res_dir+'raw/EX01-mpich3-approx2/comm-comp-report.csv': {
-        #     'key': 'ex01-mpich3apprx',
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-        
-        # '{}/raw/{}/mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-mpich3'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        '{}/raw/{}/mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-            'key': '{}-mvapich2'.format(case),
+        '{}/raw/{}/tp-mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
+            'key': '{}'.format(case),
             'lines': {
-                'omp': ['10'],
-                'type': ['total']
+                'mpi': ['mpich3', 'mvapich2'],
+                'lb': ['interval', 'reportonly'],
+                'approx': ['0', '2'],
+                'lba': ['100', '500'],
+                'type': ['total'],
             }
         },
-
-
-        # '{}/raw/{}/lb-mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-lbmpich3'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # '{}/raw/{}/lb-mpich3-approx2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-lbmpich3apprx'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        '{}/blond-meeting/{}/lb-mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-            'key': '{}-lbmvapich2'.format(case),
+        '{}/raw/{}/tp-mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
+            'key': '{}'.format(case),
             'lines': {
-                'omp': ['10'],
-                'type': ['total']
+                'mpi': ['mpich3', 'mvapich2'],
+                'lb': ['interval', 'reportonly'],
+                'approx': ['0', '2'],
+                'lba': ['100', '500'],
+                'type': ['total'],
             }
         },
-
-        # '{}/raw/{}/lb-mvapich2-approx2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-lbmvapich2apprx'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-        # '{}/raw/{}/dynamic-lb-mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-dynlbmpich3'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # '{}/raw/{}/dynamic-lb-mpich3-approx2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-dynlbmpich3apprx'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # '{}/raw/{}/dynamic-lb-mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-dynlbmvapich2'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-        # '{}/raw/{}/dynamic-lb-mvapich2-approx2/comm-comp-report.csv'.format(res_dir, case.upper()): {
-        #     'key': '{}-dynlbmvapich2apprx'.format(case),
-        #     'lines': {
-        #         'omp': ['10'],
-        #         'type': ['total']
-        #     }
-        # },
-
-    },
-    'labels': {
-
-        '{}-mpich3'.format(case): '{}-mpich3'.format(case),
-        '{}-lbmpich3'.format(case): '{}-lb-mpich3'.format(case),
-        '{}-mpich3apprx'.format(case): '{}-mpich3-approx2'.format(case),
-        '{}-dynlbmpich3'.format(case): '{}-dynamic-lb-mpich3'.format(case),
-        '{}-lbmpich3apprx'.format(case): '{}-lb-mpich3-approx2'.format(case),
-        '{}-dynlbmpich3apprx'.format(case): '{}-dynamic-lb-mpich3-approx2'.format(case),
-
-        '{}-mvapich2'.format(case): '{}-mvapich2'.format(case),
-        '{}-lbmvapich2'.format(case): '{}-lb-mvapich2'.format(case),
-        '{}-mvapich2apprx'.format(case): '{}-mvapich2-approx2'.format(case),
-        '{}-dynlbmvapich2'.format(case): '{}-dynamic-lb-mvapich2'.format(case),
-        '{}-lbmvapich2apprx'.format(case): '{}-lb-mvapich2-approx2'.format(case),
-        '{}-dynlbmvapich2apprx'.format(case): '{}-dynamic-lb-mvapich2-approx2'.format(case),
-
-        '{}-openmpi3'.format(case): '{}-openmpi3'.format(case),
-        '{}-lbopenmpi3'.format(case): '{}-lb-openmpi3'.format(case),
-        '{}-openmpi3apprx'.format(case): '{}-openmpi3-approx2'.format(case),
-        '{}-dynlbopenmpi3'.format(case): '{}-dynamic-lb-openmpi3'.format(case),
-        '{}-lbopenmpi3apprx'.format(case): '{}-lb-openmpi3-approx2'.format(case),
-        '{}-dynlbopenmpi3apprx'.format(case): '{}-dynamic-lb-openmpi3-approx2'.format(case),
-
+        '{}/raw/{}/lb-tp-mvapich2/comm-comp-report.csv'.format(res_dir, case.upper()): {
+            'key': '{}'.format(case),
+            'lines': {
+                'mpi': ['mpich3', 'mvapich2'],
+                'lb': ['interval', 'reportonly'],
+                'approx': ['0', '2'],
+                'lba': ['100', '500'],
+                'type': ['total'],
+            }
+        },
+        '{}/raw/{}/lb-tp-mpich3/comm-comp-report.csv'.format(res_dir, case.upper()): {
+            'key': '{}'.format(case),
+            'lines': {
+                'mpi': ['mpich3', 'mvapich2'],
+                'lb': ['interval', 'reportonly'],
+                'approx': ['0', '2'],
+                'lba': ['100', '500'],
+                'type': ['total'],
+            }
+        },
     },
     'markers': {
         'ex01': 'd',
@@ -203,44 +118,25 @@ config = {
         'ps': '--'
     },
     'colors': {
-        'mpich3': 'xkcd:light yellow',
-        'lbmpich3': 'xkcd:yellow',
-        # 'mpich3apprx': 'xkcd:light green',
-        'lbmpich3apprx': 'xkcd:green',
+        'mpich3-wlb': 'xkcd:light green',
+        'mpich3-wolb': 'xkcd:green',
 
-        'mvapich2': 'xkcd:light orange',
-        'lbmvapich2': 'xkcd:orange',
-        # 'mvapich2apprx': 'xkcd:light red',
-        'lbmvapich2apprx': 'xkcd:red',
-
-        # 'openmpi3': 'xkcd:light pink',
-        # 'lbopenmpi3': 'xkcd:pink',
-        # 'openmpi3apprx': 'xkcd:light purple',
-        # 'lbopenmpi3apprx': 'xkcd:purple',
-
-        'dynlbmpich3': 'xkcd:light pink',
-        'dynlbmpich3apprx': 'xkcd:pink',
-
-        'dynlbmvapich2': 'xkcd:light purple',
-        'dynlbmvapich2apprx': 'xkcd:purple',
-
-
+        'mvapich2-wlb': 'xkcd:light orange',
+        'mvapich2-wolb': 'xkcd:orange',
     },
     'hatches': {
         'mpich3': 'x',
         'openmpi3': '-',
         'mvapich2': 'o',
-        # 'ex01': '\\',
-        # 'lhc': '/',
-        # 'sps': 'o',
-        # 'ps': 'x',
     },
     'reference': {
         'ex01': {'time': 21.4, 'ppb': 1000000, 'turns': 2000},
         'sps': {'time': 430., 'ppb': 4000000, 'turns': 100},
-        'lhc': {'time': 2120., 'ppb': 2000000, 'turns': 1000},
+        # 'lhc': {'time': 2120., 'ppb': 2000000, 'turns': 1000},
+        'lhc': {'time': 350.831, 'ppb': 2000000, 'b': 48, 'turns': 500},
         'ps': {'time': 1623.7, 'ppb': 4000000, 'turns': 2000},
     },
+    # 'sequence': ['mpich3']
 
     # 'exclude': [['v1', 'notcm'], ['v2', 'notcm'], ['v4', 'notcm']],
     'x_name': 'n',
@@ -265,21 +161,21 @@ config = {
         'pad': 1, 'top': 1, 'bottom': 1, 'left': 1,
         'direction': 'inout', 'length': 3, 'width': 0.5,
     },
-    'image_name': '{}/{}-task-parallel.pdf'.format(images_dir, case),
+    'image_name': '{}/{}-tp.pdf'.format(images_dir, case),
 
 }
 
 if __name__ == '__main__':
     plots_dir = {}
-    for file in config['files'].keys():
+    for file, conf in config['files'].items():
         # print(file)
         data = np.genfromtxt(file, delimiter='\t', dtype=str)
         header, data = list(data[0]), data[1:]
-        temp = get_plots(header, data, config['files'][file]['lines'],
-                         exclude=config['files'][file].get('exclude', []))
-        temp[config['files'][file]['key']] = temp['10-total']
-        del temp['10-total']
-        plots_dir.update(temp)
+        temp = get_plots(header, data, conf['lines'],
+                         exclude=conf.get('exclude', []),
+                         prefix=True)
+        for key in temp.keys():
+            plots_dir['{}-{}'.format(conf['key'], key)] = temp[key].copy()
 
     fig = plt.figure(figsize=config['figsize'])
 
@@ -292,86 +188,79 @@ if __name__ == '__main__':
     pos = 0
     step = 0.1
     width = 1. / (len(plots_dir.keys())+1)
-    for case in ['lhc', 'sps', 'ps', 'ex01']:
-        for mpiv in ['mpich3', 'lbmpich3',
-                     'mpich3apprx', 'lbmpich3apprx',
-                     'dynlbmpich3', 'dynlbmpich3apprx',
-                     'mvapich2', 'lbmvapich2',
-                     'mvapich2apprx', 'lbmvapich2apprx',
-                     'dynlbmvapich2', 'dynlbmvapich2apprx',
-                     'openmpi3', 'lbopenmpi3',
-                     'openmpi3apprx', 'lbopenmpi3apprx']:
 
-            key = '{}-{}'.format(case, mpiv)
-            if key not in plots_dir:
-                continue
-            if 'mpich3' in mpiv:
-                version = 'mpich3'
-            elif 'mvapich2' in mpiv:
-                version = 'mvapich2'
-            elif 'openmpi3' in mpiv:
-                version = 'openmpi3'
-            values = plots_dir[key]
+    for k in sorted(plots_dir.keys()):
+        values = plots_dir[k]
+        case = k.split('-')[0]
+        mpiv = k.split('-mpi')[1].split('_')[0]
+        lb = k.split('lb')[1].split('_')[0]
+        lba = k.split('lba')[1].split('_')[0]
+        approx = k.split('approx')[1].split('_')[0]
 
-            label = config['labels'][key]
+        # key = '{}-{}-{}'.format(case, mpiv, lb)
 
-            x = np.array(values[:, header.index(config['x_name'])], float)
-            omp = np.array(
-                values[:, header.index(config['omp_name'])], float)
+        label = '{}-{}-{}-{}'.format(mpiv, lb, lba, approx)
+        # color = config['colors']['{}-{}'.format(mpiv, lb)]
+        # hatch = config['hatches'][mpiv]
+        # marker = config['markers'][case]
+        # ls = config['ls'][case]
 
-            y = np.array(values[:, header.index(config['y_name'])], float)
-            parts = np.array(values[:, header.index('ppb')], float)
-            turns = np.array(values[:, header.index('turns')], float)
-            # This is the throughput
-            y = parts * turns / y
+        x = get_values(values, header, config['x_name'])
+        omp = get_values(values, header, config['omp_name'])
+        y = get_values(values, header, config['y_name'])
+        parts = get_values(values, header, 'ppb')
+        bunches = get_values(values, header, 'b')
+        turns = get_values(values, header, 't')
 
-            # Now the reference, 1thread
-            yref = config['reference'][case]['time']
-            partsref = config['reference'][case]['ppb']
-            turnsref = config['reference'][case]['turns']
-            yref = partsref * turnsref / yref
+        # This is the throughput
+        y = parts * bunches * turns / y
 
-            speedup = y / yref
+        # Now the reference, 1thread
+        yref = config['reference'][case]['time']
+        partsref = config['reference'][case]['ppb']
+        bunchesref = config['reference'][case]['b']
+        turnsref = config['reference'][case]['turns']
+        yref = partsref * bunchesref * turnsref / yref
 
-            if len(config['x_to_keep']) < len(x):
-                x_new = []
-                speedup_new = []
-                omp_new = []
-                for i in range(len(x)):
-                    if x[i] in config['x_to_keep']:
-                        x_new.append(x[i])
-                        speedup_new.append(speedup[i])
-                        omp_new.append(omp[i])
-                x = np.array(x_new)
-                speedup = np.array(speedup_new)
-                omp = np.array(omp_new)
+        speedup = y / yref
 
-            x = x * omp
+        if len(config['x_to_keep']) < len(x):
+            x_new = []
+            speedup_new = []
+            omp_new = []
+            for i in range(len(x)):
+                if x[i] in config['x_to_keep']:
+                    x_new.append(x[i])
+                    speedup_new.append(speedup[i])
+                    omp_new.append(omp[i])
+            x = np.array(x_new)
+            speedup = np.array(speedup_new)
+            omp = np.array(omp_new)
 
-            # efficiency = 100 * speedup / x
-            plt.bar(np.arange(len(x)) + pos, speedup, width=width,
-                    color=config['colors'][mpiv],
-                    # label=label,
-                    edgecolor='0.3',
-                    # alpha=0.8,
-                    hatch=config['hatches'][version])
-            pos += width
-        pos += width * step
+        x = x * omp
+
+        # efficiency = 100 * speedup / x
+        plt.bar(np.arange(len(x)) + pos, speedup, width=width,
+                edgecolor='0.3', label=label)
+        # hatch=hatch)
+        pos += width
+    pos += width * step
 
     plt.xticks(np.arange(len(x)) + pos/2.2, np.array(x//10, int))
 
-    handles = []
-    for k, v in config['colors'].items():
-        patch = mpatches.Patch(label=k, edgecolor='black', facecolor=v,
-                               linewidth=.5, alpha=0.9)
-        handles.append(patch)
+    # handles = []
+    # for k, v in config['colors'].items():
+    #     patch = mpatches.Patch(label=k, edgecolor='black', facecolor=v,
+    #                            linewidth=.5, alpha=0.9)
+    #     handles.append(patch)
 
-    for k, v in config['hatches'].items():
-        patch = mpatches.Patch(label=k, edgecolor='black',
-                               facecolor='0.8', hatch=v, linewidth=.5,)
-        handles.append(patch)
+    # for k, v in config['hatches'].items():
+    #     patch = mpatches.Patch(label=k, edgecolor='black',
+    #                            facecolor='0.8', hatch=v, linewidth=.5,)
+    #     handles.append(patch)
 
-    plt.legend(handles=handles, **config['legend'])
+    # plt.legend(handles=handles, **config['legend'])
+    plt.legend(**config['legend'])
     plt.gca().tick_params(**config['tick_params'])
 
     plt.subplots_adjust(**config['subplots_adjust'])
