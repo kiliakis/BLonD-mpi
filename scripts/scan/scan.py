89740e4d1ca7361288cc365a3f4fb481ef1e4b79
Konstantinos Iliakis <konstantinos.iliakis@cern.ch>
2020-04-09 18:57:15 +0200

    debugged scan locally

--- scripts/scan/scan.py
+++ scripts/scan/scan.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import random
 import yaml
 import argparse
-import math
+import numpy as np
 
 import common
 
@@ -15,10 +15,10 @@ this_filename = sys.argv[0].split('/')[-1]
 parser = argparse.ArgumentParser(description='Run locally the MPI experiments.',
                                  usage='python {} -t lhc sps ps'.format(this_filename[:-3]))
 
-parser.add_argument('-environment', '--environment', type=str, default='local', choices=['local', 'cluster'],
+parser.add_argument('-e', '--environment', type=str, default='local', choices=['local', 'cluster'],
                     help='The environment to run the scan.')
 
-parser.add_argument('-t', '--testcases', type=str, default=['lhc', 'sps', 'ps'], choices=['lhc', 'sps', 'ps'],
+parser.add_argument('-t', '--testcases', type=str, default=['lhc,sps,ps'],
                     help='Which testcases to run. Default: all')
 
 parser.add_argument('-o', '--output', type=str, default='./results/local',
@@ -29,23 +29,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
     top_result_dir = args.output
 
-    for tc in args.testcases:
-        yc = yaml.load(
-            open(this_directory + '/{}_configs.yml'.format(tc), 'r'))[args.environment]
+    for tc in args.testcases.split(','):
+        yc = yaml.load(open(this_directory + '/{}_configs.yml'.format(tc), 'r'),
+                       Loader=yaml.FullLoader)[args.environment]
 
         result_dir = top_result_dir + '/{}/{}/{}/{}/{}'
 
         job_name_form = '_p{}_b{}_s{}_t{}_w{}_o{}_N{}_red{}_mtw{}_seed{}_approx{}_mpi{}_lb{}_lba{}_monitor{}_tp{}_'
 
         total_sims = 0
-        for rc in yc.run_configs:
-            total_sims += yc.configs[rc]['repeats'] * len(yc.configs[rc]['w'])
+        for rc in yc['run_configs']:
+            total_sims += yc['configs'][rc]['repeats'] * \
+                len(yc['configs'][rc]['workers'])
 
         print("Total runs: ", total_sims)
         current_sim = 0
 
-        for analysis in yc.run_configs:
-            config = yc.configs[analysis]
+        for analysis in yc['run_configs']:
+            config = yc['configs'][analysis]
             ps = config['particles']
             bs = config['bunches']
             ss = config['slices']
@@ -56,9 +57,9 @@ if __name__ == '__main__':
             exes = config['exe']
             times = config['time']
             # partitions = config['partition']
-            mtws = config['mtw']
-            ms = config['monitor']
-            seeds = config['seed']
+            mtws = config.get('mtw', ['0']*len(ps))
+            ms = config.get('monitor', ['0']*len(ps))
+            seeds = config.get('seed', ['0']*len(ps))
             approxs = config['approx']
             timings = config['timing']
             mpis = config['mpi']
@@ -76,7 +77,7 @@ if __name__ == '__main__':
                             exes, approxs, timings, mpis,
                             logs, lbs, lbas, tps):
 
-                N = int(max(math.ceil(w * o / common.cores_per_cpu), 1))
+                N = int(max(np.ceil(w * o / common.cores_per_cpu), 1))
 
                 job_name = job_name_form.format(p, b, s, t, w, o, N,
                                                 r, mtw, seed, approx, mpi,
@@ -86,21 +87,22 @@ if __name__ == '__main__':
                     timestr = datetime.now().strftime('%d%b%y.%H-%M-%S')
                     timestr = timestr + '-' + str(random.randint(0, 100))
                     output = result_dir.format(
-                        yc.case, analysis, job_name, timestr, 'output.txt')
+                        tc, analysis, job_name, timestr, 'output.txt')
                     error = result_dir.format(
-                        yc.case, analysis, job_name, timestr, 'error.txt')
+                        tc, analysis, job_name, timestr, 'error.txt')
                     monitorfile = result_dir.format(
-                        yc.case, analysis, job_name, timestr, 'monitor')
+                        tc, analysis, job_name, timestr, 'monitor')
                     log_dir = result_dir.format(
-                        yc.case, analysis, job_name, timestr, 'log')
+                        tc, analysis, job_name, timestr, 'log')
                     report_dir = result_dir.format(
-                        yc.case, analysis, job_name, timestr, 'report')
+                        tc, analysis, job_name, timestr, 'report')
                     for d in [log_dir, report_dir]:
                         if not os.path.exists(d):
                             os.makedirs(d)
-                            
+
                     # For the extract script
-                    open(os.path.join(top_result_dir, yc.case, analysis, '.analysis'), 'a').close()
+                    open(os.path.join(top_result_dir, tc,
+                                      analysis, '.analysis'), 'a').close()
 
                     if args.environment == 'local':
                         batch_args = [common.mpirun, '-n', str(w)]
@@ -109,12 +111,12 @@ if __name__ == '__main__':
                             common.batchsubmit,
                             common.nodes, str(N),
                             common.workers, str(w),
-                            common.tasks_per_node, str(math.ceil(w/N)),
+                            common.tasks_per_node, str(int(np.ceil(w/N))),
                             common.cores, str(o),  # str(o),
                             common.time, str(time),
                             common.output, output,
                             common.error, error,
-                            common.jobname, yc.case + '-' + analysis + job_name.split('/')[0] + '-' + str(i)]
+                            common.jobname, tc + '-' + analysis + job_name.split('/')[0] + '-' + str(i)]
                         batch_args += common.default_batch_args
                         batch_args += [common.batch_script, common.batchrun]
 
@@ -131,8 +133,8 @@ if __name__ == '__main__':
                         '--reduce', str(r),
                         '--mtw', str(mtw),
                         '--approx', str(approx),
-                        '-loadbalance', lb, '-loadbalancearg', str(lba),
-                        '--withtp', tp,
+                        '--loadbalance', lb, '--loadbalancearg', str(lba),
+                        '--withtp', str(tp),
                         '--log', str(log), '--logdir', log_dir]
 
                     print(job_name, timestr)
